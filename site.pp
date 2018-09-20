0c3cf73a33b2eb7fc93a395001ef1197b3f523fd
Nick Moon <nmoon@thepublicopinion.com>
2018-09-20 11:09:28 -0500

    Added MOTD
    all hail knife crab

--- site.pp
+++ site.pp
@@ -1,1 +1,11 @@
 hiera_include('classes')
+
+
+node default {
+    file { '/etc/motd':
+        owner => 'root',
+        group => 'root',
+        mode => '0644' ,
+        content => "\nAll Hail the Knife Crab\n"
+    }
+}
